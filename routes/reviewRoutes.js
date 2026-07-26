6ca6df651d981705ca1c9afd0314e164492ab6b4
AMIRALI <alimoradi7718@gmail.com>
2026-07-26 17:24:15 +0330

    feat: enhance route protection for review creation and deletion

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,21 +4,27 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
-    reviewController.createNewReview,
+    reviewController.setTourUserIds,
+    reviewController.createReview,
   );
 
 router
   .route('/:id')
+  .get(reviewController.getReview)
   .patch(
     authController.restrictTo('user', 'admin'),
     reviewController.updateReview,
   )
-  .delete(reviewController.deleteReview);
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview,
+  );
 
 module.exports = router;
