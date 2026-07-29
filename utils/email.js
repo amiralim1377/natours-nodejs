7945a03b55d0ccd5b23eda78723a71def82d61c3
AMIRALI <alimoradi7718@gmail.com>
2026-07-29 11:55:55 +0330

    refactor: comment out production email transport code for clarity

--- utils/email.js
+++ utils/email.js
@@ -11,10 +11,10 @@ module.exports = class Email {
   }
 
   newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-      // SENDGRID
-      return 1;
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   // SENDGRID
+    //   return 1;
+    // }
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
